dffc5ef0adacef88a09f7048fa04a7438d74ff25
Luigi <a>
2014-03-03 03:54:18 +0100

    - Added java test exception class.

---
+++ src-test-unit/NopAdderInstr.hpp
@@ -0,0 +1,108 @@
+#ifndef __BCI__INSTR_VISITOR__HPP__
+#define	__BCI__INSTR_VISITOR__HPP__
+
+#include "jnif.hpp"
+
+using namespace jnif;
+
+template<typename TVisitor>
+class InstrVisitor: public ClassForwardVisitor<TVisitor> {
+	typedef ClassForwardVisitor<TVisitor> base;
+	using base::cv;
+public:
+
+	class Method: public base::Method {
+		using base::Method::mv;
+	public:
+
+		InstrVisitor& instr;
+
+		Method(typename TVisitor::Method& mv, InstrVisitor& instr) :
+				base::Method(mv), instr(instr) {
+		}
+
+		class Code: public base::Method::Code {
+			using base::Method::Code::bv;
+		public:
+			InstrVisitor& instr;
+			Code(typename TVisitor::Method::Code& bv, InstrVisitor& instr) :
+					base::Method::Code(bv), instr(instr) {
+			}
+
+			inline void enter(u2 maxStack, u2 maxLocals) {
+				bv.enter(maxStack, maxLocals);
+
+				const std::string& methodName = instr.cp->getUtf8(instr.nameIndex);
+
+				if (methodName == "<init>321") {
+					bv.visitZero(-42, OPCODE_nop);
+
+					bv.visitZero(-87, OPCODE_aload_0);
+
+					bv.visitInvoke(-32, OPCODE_invokestatic,
+							instr.methodRefIndex, "hola", "q", "tal");
+				}
+			}
+
+			inline void visitNewArray(int offset, u1 opcode, u1 atype) {
+				// FORMAT: newarray atype
+				// OPERAND STACK: ... | count: int -> ... | arrayref
+
+				// STACK: ... | count
+
+				bv.visitZero(offset, OPCODE_dup);
+				// STACK: ... | count | count
+
+				bv.visitNewArray(offset, opcode, atype); // newarray
+				// STACK: ... | count | arrayref
+
+				bv.visitZero(offset, OPCODE_dup_x1);
+				// STACK: ... | arrayref | count | arrayref
+
+				bv.visitBiPush(offset, OPCODE_bipush, atype);
+				//u2 typeindex = instr.cp->addInteger(atype);
+
+				//bv.visitLdc(offset, OPCODE_ldc_w, typeindex);
+				// STACK: ... | arrayref | count | arrayref | atype
+
+				bv.visitInvoke(offset, OPCODE_invokestatic,
+						instr.methodRefIndex2, "", "", "");
+				// STACK: ... | arrayref
+
+			}
+
+			inline void visitAttr(u2 nameIndex, u4 len, const u1* data) {
+				const std::string& attrName = instr.cp->getUtf8(nameIndex);
+				if (attrName != "StackMapTable") {
+					bv.visitAttr(nameIndex, len, data);
+				}
+			}
+		};
+
+		Code visitCode(u2 nameIndex) {
+			auto bv = mv.visitCode(nameIndex);
+			return Code(bv, instr);
+
+		}
+	};
+
+	InstrVisitor(TVisitor& cv) :
+			ClassForwardVisitor<TVisitor>(cv) {
+	}
+
+	inline Method visitMethod(u2 accessFlags, u2 nameIndex, u2 descIndex) {
+
+		this->nameIndex = nameIndex;
+		typename TVisitor::Method mv = base::cv.visitMethod(accessFlags,
+				nameIndex, descIndex);
+
+		return Method(mv, *this);
+	}
+
+	ConstPool* cp;
+	u2 nameIndex;
+	u2 methodRefIndex;
+	u2 methodRefIndex2;
+};
+
+#endif
--- src-test-unit/test-unit.cpp
+++ src-test-unit/test-unit.cpp
@@ -13,48 +13,10 @@ using namespace jnif;
 
 extern u1 jnif_BasicClass_class[];
 extern u4 jnif_BasicClass_class_len;
-//
-//void FrInstrClassFile(unsigned char* classFile, int classFileLen,
-//		const char* className, unsigned int* new_class_data_len,
-//		unsigned char** new_class_data) {
-//
-//	auto instr =
-//			[&](ostream& os) {
-//
-//				ClassWriterVisitor<> cwv(cout);
-//				ClassPrinterVisitor<decltype(cwv)> cpv(os, className, classFileLen, cwv);
-//
-//				BufferReader br(classFile, classFileLen);
-//				ClassParser::parse(br,cpv);
-//
-//				int len = cwv.getClassFileSize(cwv.cf);
-//
-//				ASSERT(classFileLen == len, "%d must be equal to %d on class %s",
-//						classFileLen, len, className);
-//				return;
-//				*new_class_data_len = len;
-//				*new_class_data = (u1*)malloc(len);
-//
-//				BufferWriter bw(*new_class_data, len);
-//				cwv.writeClassFile(bw, cwv.cf);
-//
-//				return;
-//
-//				for (int i = 0; i < len; i++) {
-//					if (classFile[i] != (*new_class_data)[i]) {
-//
-//						ASSERT(false, "error on %d: %d:%d != %d:%d", i,
-//								classFile[i],classFile[i+1],
-//								(*new_class_data)[i],(*new_class_data)[i+1]
-//						);
-//					}
-//				}
-//
-//				// TODO: free of *new_class_data.
-//			};
-//
-//	instr(cerr);
-//}
+
+extern u1 jnif_ExceptionClass_class[];
+extern u4 jnif_ExceptionClass_class_len;
+
 //
 //void testSimpleModel() {
 //	ClassPrinterVisitor<> c(cout, "Hola", 123);
@@ -79,51 +41,70 @@ extern u4 jnif_BasicClass_class_len;
 //	cd.visitJump(-23, OPCODE_ifeq, -1);
 //}
 //
-//void testIdentityParserWriter() {
-//	auto instr =
-//			[&](unsigned char* classFile, int classFileLen, const char* className) {
-//				ClassWriterVisitor<> cw(cout);
-//
-//				BufferReader br(classFile, classFileLen);
-//				ClassParser::parse(br,cw);
-//
-//				int len = cw.getClassFileSize(cw.cf);
-//				ASSERT(classFileLen == len, "%d must be equal to %d on class %s",
-//						classFileLen, len, className);
-//			};
-//
-//	instr(jnif_BasicClass_class, jnif_BasicClass_class_len, "jnif/BasicClass");
-//}
-//
-//void testParser() {
-//	u1* newClassData;
-//	u4 newClassDataLen;
-//
-//	FrInstrClassFile(jnif_BasicClass_class, jnif_BasicClass_class_len,
-//			"jnif/BasicClass", &newClassDataLen, &newClassData);
-//}
 
-void testWriter() {
-	unsigned char* classFile = jnif_BasicClass_class;
-	int classFileLen = jnif_BasicClass_class_len;
-	const char* className = "jnif/BasicClass";
+void testIdentityComputeSize() {
+	auto instr =
+			[&](unsigned char* classFile, int classFileLen, const char* className) {
+				BufferSize w;
+				BufferReader r(classFile, classFileLen);
+				ClassParser::Writer<BufferSize> cw(w);
+				ClassParser::parse(r, cw);
 
-	BufferReader br(classFile, classFileLen);
+				int len = w.size();
+				ASSERT(classFileLen == len, "Expected class file len %d, actual was %d, on class %s",
+						classFileLen, len, className);
+			};
 
-	BufferSize w;
-	ClassParser::Writer<BufferSize> cw(w);
-	ClassParser::parse(br, cw);
+	instr(jnif_BasicClass_class, jnif_BasicClass_class_len, "jnif/BasicClass");
+	instr(jnif_ExceptionClass_class, jnif_ExceptionClass_class_len,
+			"jnif/ExceptionClass");
+}
 
-	int len = w.size();
-	ASSERT(classFileLen == len, "Expected class file len %d, actual was %d, on class %s",
-			classFileLen, len, className);
+void testIdentityParserWriter() {
+	auto instr =
+			[&](unsigned char* classFile, int classFileLen, const char* className) {
+
+				int new_class_data_len;
+				{
+					BufferSize w;
+					BufferReader br(classFile, classFileLen);
+					ClassParser::Writer<BufferSize> cw(w);
+					ClassParser::parse(br, cw);
+					new_class_data_len = w.size();
+				}
+
+				ASSERT(classFileLen == new_class_data_len, "Expected class file len %d, actual was %d, on class %s",
+						classFileLen, new_class_data_len, className);
+
+				u1* new_class_data = new u1[new_class_data_len];
+
+				{
+					BufferWriter w(new_class_data, new_class_data_len);
+					BufferReader r(classFile, classFileLen);
+					ClassParser::Writer<BufferWriter> cw(w);
+					ClassParser::parse(r, cw);
+				}
+
+				for (int i = 0; i < new_class_data_len; i++) {
+					ASSERT(classFile[i] == new_class_data[i], "error on %d: %d:%d != %d:%d", i,
+							classFile[i],classFile[i+1],
+							new_class_data[i],new_class_data[i+1]
+					);
+				}
+
+				delete [] new_class_data;
+			};
+
+	instr(jnif_BasicClass_class, jnif_BasicClass_class_len, "jnif/BasicClass");
+	instr(jnif_ExceptionClass_class, jnif_ExceptionClass_class_len,
+			"jnif/ExceptionClass");
+
 }
 
 int main(int argc, const char* argv[]) {
+	testIdentityComputeSize();
+	testIdentityParserWriter();
 	//testSimpleModel();
-	//testIdentityParserWriter();
-	//testParser();
-	testWriter();
 
 	printf("argc: %d, %d\n", argc, jnif_BasicClass_class_len);
 	return 0;
