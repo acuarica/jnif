19a327f4c7248d7914aaefc7ea748170cce6ea8f
Luigi <a>
2014-05-20 11:21:38 +0200

    Added rt from JDK for testing.
    Former-commit-id: 5c4190decd5d5fcc73304f72eb95029e8c4d7beb

---
+++ src/ClassFile.cpp
@@ -0,0 +1,48 @@
+/*
+ * Frame.cpp
+ *
+ *  Created on: May 19, 2014
+ *      Author: luigi
+ */
+#include "jnif.hpp"
+#include "jnifex.hpp"
+
+namespace jnif {
+
+Attrs::~Attrs() {
+	for (Attr* attr : attrs) {
+		delete attr;
+	}
+}
+
+CodeAttr::~CodeAttr() {
+	if (cfg != nullptr) {
+		delete cfg;
+	}
+}
+
+ClassFile::~ClassFile() {
+	for (Field* field : fields) {
+		delete field;
+	}
+
+	for (Method* method : methods) {
+		delete method;
+	}
+}
+
+Field* ClassFile::addField(ConstIndex nameIndex, ConstIndex descIndex,
+		u2 accessFlags) {
+	Field* field = new Field(accessFlags, nameIndex, descIndex, this);
+	fields.push_back(field);
+	return field;
+}
+
+Method* ClassFile::addMethod(ConstIndex nameIndex, ConstIndex descIndex,
+		u2 accessFlags) {
+	Method* method = new Method(accessFlags, nameIndex, descIndex, this);
+	methods.push_back(method);
+	return method;
+}
+
+}
