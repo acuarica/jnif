3c1e4c8abf8cbc8f1290a3575bb078408c1aa53e
Luigi <a>
2014-06-03 23:24:52 +0200

    Added performance timer in java side to make a better comparison in a Linux box. Refactor Makefile: merge tasks eval-dacapo and eval-scala.
    Former-commit-id: 9486b1c012827124f93740391b13b724c4a89956

--- src-instrserver/src/ch/usi/inf/sape/frheap/server/FrHeapInstrumentWorker.java
+++ src-instrserver/src/ch/usi/inf/sape/frheap/server/FrHeapInstrumentWorker.java
@@ -1,7 +1,9 @@
 package ch.usi.inf.sape.frheap.server;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.log4j.Logger;
 
@@ -16,10 +18,19 @@ public class FrHeapInstrumentWorker extends Thread {
 
 	private FrHeapInstrumenter _instr;
 
+	private PrintWriter _writer;
+
+	private String _p;
+
 	public FrHeapInstrumentWorker(FrHeapInstrumentSocket socket,
-			FrHeapInstrumenter instr) {
+			FrHeapInstrumenter instr, int tid, String p)
+			throws FileNotFoundException, UnsupportedEncodingException {
 		_socket = socket;
 		_instr = instr;
+
+		_p = p;
+		_writer = new PrintWriter("build/eval-server-" + p + "-" + tid
+				+ ".prof");
 	}
 
 	public void run() {
@@ -57,18 +68,38 @@ public class FrHeapInstrumentWorker extends Thread {
 
 			logger.debug(String.format("Worker done"));
 		} catch (IOException e) {
+
 			logger.error("Error on worker. Exiting...", e);
 
 			throw new RuntimeException("Error on worker.", e);
+		} finally {
+			// _writer.flush();
+			_writer.close();
 		}
 	}
 
 	private byte[] instrumentClass(String className, byte[] classBytes)
 			throws IOException {
+		byte[] res;
+		long instrTime;
+
 		try {
-			return _instr.instrumentClass(classBytes, className);
+			long startTime = System.nanoTime();
+			res = _instr.instrumentClass(classBytes, className);
+			long endTime = System.nanoTime();
+			instrTime = endTime - startTime;
 		} catch (RuntimeException ex) {
 			return classBytes;
 		}
+
+		prof(className, instrTime);
+
+		return res;
+	}
+
+	private void prof(String className, long instrTime) {
+		double time = (double) instrTime / (1000 * 1000 * 1000);
+
+		_writer.format("%s,%s,%f\n", _p, className, time);
 	}
 }
