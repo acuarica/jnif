bf4ed9a619aa7d0ddfab056bb346da437d4de3e5
Luigi <luismastrangelo@gmail.com>
2020-02-08 14:42:07 +0100

    Clean main

--- rust/jnif/src/main.rs
+++ rust/jnif/src/main.rs
@@ -1,42 +1,13 @@
-
 use std::io::*;
 use std::fs::*;
-use std::io::prelude::*;
-
 
 enum ConstPool {
-
 }
-
-fn main2() -> std::io::Result<()> {
-    println!("Hello, world!");
-    // let mut file = File::open("test/Test.java")?;
-    let mut file = File::open("test/Main.class")?;
-    let mut data = Vec::new();
-    file.read_to_end(&mut data);
-    println!("{}", data[0]);
-    println!("{}", data[1]);
-    println!("{}", data[2]);
-    println!("{}", data[3]);
-
-    println!("Hello, world!");
-    // println!("{}", contents);
-    Ok(())
-}
-
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
-    // let mut file = File::open("test/Test.java")?;
     let mut file = File::open("test/Main.class")?;
     let mut data = Vec::new();
     file.read_to_end(&mut data);
-    println!("{}", data[0]);
-    println!("{}", data[1]);
-    println!("{}", data[2]);
-    println!("{}", data[3]);
-
-    println!("Hello, world!");
-    // println!("{}", contents);
+    println!("Magic: {:x} {:x} {:x} {:x}", data[0], data[1], data[2], data[3]);
     Ok(())
 }
