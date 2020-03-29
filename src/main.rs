484f2c243ff64a3ba16cd3d0bc599901b87ebff1
Luigi <luismastrangelo@gmail.com>
2020-03-30 01:44:48 +0200

    Use bytes::Buf to read a file buffer with a cursor
    Improvement to read until the Constant Pool count.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::fs;
 // use std::fs::*;
 
+use bytes::Buf;
+
 // enum ConstPool {
 // }
 
@@ -10,6 +12,19 @@ fn main() -> std::io::Result<()> {
     // let mut data = Vec::new();
     let data = fs::read("test/Main.class")?;
     // file.read_to_end(&mut data);
+    let mut c: &[u8] = &data;
+    let magic = c.get_u32();
+    assert_eq!(0xcafebabe, magic);
+
+    let minor = c.get_u16();
+    let major = c.get_u16();
+
+    let count = c.get_u16();
+
+    println!("Magic: {:x}", magic);
+    println!("major.minor: {}.{}", major, minor);
+    println!("CP count: {}", count);
+
     println!("Magic: {:x} {:x} {:x} {:x}", data[0], data[1], data[2], data[3]);
     Ok(())
 }
