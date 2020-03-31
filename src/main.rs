d1d7444e8ec6e3642a9a5cd6d1c58fb2d293f5d1
Luigi <luismastrangelo@gmail.com>
2020-03-31 17:54:22 +0200

    Remove unused ClassFile result warning

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use jnif::parser;
 
 fn main() -> std::io::Result<()> {
     let data = fs::read("tests/Main.class")?;
-    parser::parse(&data);
+    let res = parser::parse(&data);
+
+    if let Ok(cf) = res {
+        assert_eq!(55, cf.major);
+        assert_eq!(0, cf.minor);
+        // println!("Magic: {:x}", magic);
+        println!("major.minor: {}.{}", cf.major, cf.minor);
+    }
 
     Ok(())
 }
