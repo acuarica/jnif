9999fe018239961572081da1a8a38edb9faec51a
Luigi <luismastrangelo@gmail.com>
2020-03-31 17:42:44 +0200

    Add more cases to test the CP parser

--- tests/Main.java
+++ tests/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
+    public static int INT_VALUE = 70000;
+    public static float FLOAT_VALUE = 1.3f;
+    public static long LONG_VALUE = 480L;
+    public static double DOUBLE_VALUE = 1.7d;
     public static void main(final String[] args) {
         System.out.println("Hello rust");
+        System.out.println(INT_VALUE);
+        System.out.println(FLOAT_VALUE);
+        System.out.println(LONG_VALUE);
+        System.out.println(DOUBLE_VALUE);
     }
 }
